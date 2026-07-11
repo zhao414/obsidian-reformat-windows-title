216cddd989dfb177b149fd3231e3d7d733da4447
Song, Zhao <zhaosong414@gmail.com>
2026-07-12 00:17:02 +0800

    Use activeWindow.setTimeout for popout window compatibility

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ export default class ReformatWindowsTitlePlugin extends Plugin {
 
     this.registerEvent(
       this.app.workspace.on("layout-change", () => {
-        setTimeout(() => this.updateTitle(), 0);
+        activeWindow.setTimeout(() => this.updateTitle(), 0);
       }),
     );
 
